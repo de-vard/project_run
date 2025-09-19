53b77c2f332883312ad8b1aadb6af35274a4180a
vlad <de_vlad@gmx.com>
2025-09-19 22:55:24 +0300

    добален сериализатор, роутер, вьюшка

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -12,3 +16,6 @@ def company_details(request):
         }
         return Response(details)
 
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
