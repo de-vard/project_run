d4dfe346ad8d1c79387aa5f54495fd5b4e70e0b1
vlad <de_vlad@gmx.com>
2026-02-04 16:57:03 +0300

    Задание 12. fix-4

--- app_run/views.py
+++ app_run/views.py
@@ -111,7 +111,7 @@ class AthleteInfoAPIView(views.APIView):
         }
 
         weight = request.data.get('weight')
-        if weight.isdigit():
+        if weight.isalpha():
             return Response({'error': 'Invalid weight'}, status=status.HTTP_400_BAD_REQUEST)
         if weight is not None and not (0 < int(weight) < 900):
             return Response({'error': 'Invalid weight'}, status=status.HTTP_400_BAD_REQUEST)
