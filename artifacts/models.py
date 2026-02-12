8efa4818a14565bd43fb3389ee8bb5ccc1eb1021
vlad <de_vlad@gmx.com>
2026-02-12 21:50:41 +0300

    17 задача

--- artifacts/models.py
+++ artifacts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class CollectibleItem(models.Model):
     """Модель для артефактов которые можно собрать в забеге"""
     name = models.CharField(max_length=200)
-    uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    uid = models.CharField(max_length=50, unique=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     picture = models.URLField()
