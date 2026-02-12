ab2a2c263f11298f5e4680b99a85eb55e8bbe2d6
vlad <de_vlad@gmx.com>
2026-02-12 21:46:33 +0300

    17 задача

--- artifacts/serializers.py
+++ artifacts/serializers.py
@@ -4,6 +4,22 @@ from artifacts.models import CollectibleItem
 
 
 class CollectibleItemSerializer(serializers.ModelSerializer):
+
+    def validate_value(self, value):
+        if value < 0:
+            raise serializers.ValidationError("value must be >= 0")
+        return value
+
+    def validate_latitude(self, value):
+        if not (-90 <= value <= 90):
+            raise serializers.ValidationError("latitude out of range")
+        return value
+
+    def validate_longitude(self, value):
+        if not (-180 <= value <= 180):
+            raise serializers.ValidationError("longitude out of range")
+        return value
+
     class Meta:
         model = CollectibleItem
         fields = '__all__'
